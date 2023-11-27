5183f123499b5c7316f74d20cc7b764a9e22b3fe
Daan <Daan@plds.nl>
2023-11-27 15:59:50 +0100

    Initial Commit

---
+++ Truss Calc PLDS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Truss Calc PLDS
+//
+//  Created by Daan Baanvinger on 27/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
